3d4712a57cc9455dd72a42167ebc653e9ec7d160
tungvn <vnt2908@gmail.com>
2021-12-28 23:30:48 +0700

    feat: [Code format]
    Update code format

--- src/bitcityz/components/spinner/spinner.tsx
+++ src/bitcityz/components/spinner/spinner.tsx
@@ -5,20 +5,12 @@ import '../../assets/index.css'
 function Spinner() {
   return (
     <div className="loading-container" v-if="isVisible">
-    <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
         <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-            <circle
-                className="path"
-                fill="none"
-                strokeWidth="6"
-                strokeLinecap="round"
-                cx="33"
-                cy="33"
-                r="30"
-            />
+          <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30" />
         </svg>
+      </div>
     </div>
-</div>
   )
 }
 
