cfc16f4c92a010941347f130ca8cd8ac43456682
Vu Ngoc Tung <deptraikosoai2908@gmail.com>
2021-12-29 11:24:54 +0700

    feat: [Update spinner]
    Update spinner image

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
-import PanIcon from './mexi.png'
+import PanIcon from './bitcityz.png'
 // import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from './types'
 
