68d22615eba232029a0f457a9244b2b9a729028a
tungvn <vnt2908@gmail.com>
2022-02-19 00:12:41 +0700

    feat: Remove comment
    Remove comment

--- src/bitcityz/scenes/launch_pad/components/pool_detail/About.tsx
+++ src/bitcityz/scenes/launch_pad/components/pool_detail/About.tsx
@@ -16,22 +16,7 @@ function About({ idoPool, isLoading }) {
       ) : (
         <p className="text-[#9E9E9E] mt-5 leading-5">{idoPool.baseInfo.description}</p>
       )}
-      {/* <h6 className="text-[#F5F5F5] mt-5">Play to Earn Model</h6>
-      {isLoading ? (
-        <div className="flex flex-col gap-y-1 mt-5">
-          <Skeleton width="100%" height="20px" />
-          <Skeleton width="100%" height="20px" />
-        </div>
-      ) : (
-        <p className="text-[#9E9E9E] mt-5 leading-5">{idoPool?.baseInfo.model}</p>
-      )} */}
 
-      {/* <h6 className="text-[#F5F5F5] mt-5">Highlight Features</h6>
-      <h6 className="text-[#F5F5F5] mt-1">Faction, team driven.</h6>
-      <h6 className="text-[#F5F5F5] mt-1">Play to Earn</h6>
-      <h6 className="text-[#F5F5F5] mt-1">Earn NFTs to improve your in-game character</h6>
-      <h6 className="text-[#F5F5F5] mt-1">Open World, revenue generating assets from empire building</h6>
-      <h6 className="text-[#F5F5F5] mt-1">System Requirements</h6> */}
       <h6 className="text-[#F5F5F5] mt-7">Token infomation</h6>
       <p className="grid gap-x-3 mt-3 grid-cols-[110px,auto] md:grid-cols-[150px,auto] md:gap-x-8">
         <span className="text-[#9E9E9E]">Name</span>
