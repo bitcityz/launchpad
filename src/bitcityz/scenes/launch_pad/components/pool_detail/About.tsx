1cb8ae49dd22ed81ed1511ff323f900a05ea22e6
tungvn <vnt2908@gmail.com>
2022-02-19 00:35:41 +0700

    feat: Test build
    Add class test build

--- src/bitcityz/scenes/launch_pad/components/pool_detail/About.tsx
+++ src/bitcityz/scenes/launch_pad/components/pool_detail/About.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from '@mexi/uikit'
 
 function About({ idoPool, isLoading }) {
   return (
-    <div className="relative hihi">
+    <div className="relative haha">
       <h6 className="text-[#F5F5F5]">Introduction</h6>
       {isLoading ? (
         <div className="flex flex-col gap-y-1 mt-5">
